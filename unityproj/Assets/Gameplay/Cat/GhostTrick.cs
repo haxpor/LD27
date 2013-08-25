867bff1cba1f122c25e896a405b76c7452143d94
Wasin Thonkaew <haxpor@gmail.com>
2013-08-25 17:48:59 +0700

    Improved following cat's camera. Added ghost trick activation (for very short time), and number of use limitation. Fixed bounding distance to be smaller than the actual half width/height of LevelManager. Changed movement to be based on acceleration not force.

--- unityproj/Assets/Gameplay/Cat/GhostTrick.cs
+++ unityproj/Assets/Gameplay/Cat/GhostTrick.cs
@@ -2,41 +2,52 @@
 using System.Collections;
 
 public class GhostTrick : MonoBehaviour {
+	public float ghostTrickSlowTimeScale = 0.2f;
+	public float ghostTrickActiveDuration = 0.6f;	// in secs
+	public int numberOfGhostTrickUsed = 10;
+	
+	private float ghostTrickCountingTimeActiveDuration = 0.0f;
+	private bool isGhostTrickActivated = false;
+	
+	
 	// Use this for initialization
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetButtonDown("GhostTrick"))
+		if(Input.GetButtonDown("GhostTrick") && !isGhostTrickActivated && numberOfGhostTrickUsed > 0)
 		{
-            Time.timeScale = 0.2f;
+			numberOfGhostTrickUsed--;
+			isGhostTrickActivated = true;
+			
+            Time.timeScale = ghostTrickSlowTimeScale;
             // If you lower timeScale it is recommended to also lower Time.fixedDeltaTime by the same amount.
             Time.fixedDeltaTime = 0.02F * Time.timeScale;
             gameObject.BroadcastMessage("GhostTrickOn", SendMessageOptions.DontRequireReceiver);
 		}
-        else if (Input.GetButtonUp("GhostTrick"))
-        {
-            Time.timeScale = 1.0f;
-            // If you lower timeScale it is recommended to also lower Time.fixedDeltaTime by the same amount.
-            Time.fixedDeltaTime = 0.02F * Time.timeScale;
-            gameObject.BroadcastMessage("GhostTrickOff", SendMessageOptions.DontRequireReceiver);
-        }
-		else if(Input.GetButtonDown("SpeedUp"))
-		{
-            Time.timeScale = 3.0f;
-            // If you lower timeScale it is recommended to also lower Time.fixedDeltaTime by the same amount.
-            Time.fixedDeltaTime = 0.02F * Time.timeScale;
-            gameObject.BroadcastMessage("SpeedUpOn", SendMessageOptions.DontRequireReceiver);
-		}
-        else if (Input.GetButtonUp("SpeedUp"))
-        {
-            Time.timeScale = 1.0f;
-            // If you lower timeScale it is recommended to also lower Time.fixedDeltaTime by the same amount.
-            Time.fixedDeltaTime = 0.02F * Time.timeScale;
-            gameObject.BroadcastMessage("SpeedUpOff", SendMessageOptions.DontRequireReceiver);
-        }
 		
+		// counting down time
+		if(isGhostTrickActivated)
+		{
+			ghostTrickCountingTimeActiveDuration += Time.deltaTime;
+			Debug.Log ("Ghosttrick time: " + ghostTrickCountingTimeActiveDuration);
+			
+			// if reach the active duration then go back to normal
+			if(ghostTrickCountingTimeActiveDuration >= ghostTrickActiveDuration)
+			{
+				Debug.Log ("entered");
+				
+				// reset states
+				isGhostTrickActivated = false;
+				ghostTrickCountingTimeActiveDuration = 0.0f;
+				
+				Time.timeScale = 1.0f;
+				
+	            // If you lower timeScale it is recommended to also lower Time.fixedDeltaTime by the same amount.
+	            Time.fixedDeltaTime = 0.02F * Time.timeScale;
+	            gameObject.BroadcastMessage("GhostTrickOff", SendMessageOptions.DontRequireReceiver);
+			}
+		}
 	}
 }
