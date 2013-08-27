77ec04029f9a68b8622ac1528ab353ac0eb00386
Wasin Thonkaew <haxpor@gmail.com>
2013-08-27 09:43:44 +0700

    Integrated and fixed some problem about world progression.

--- unityproj/Assets/Gameplay/Gameplay/WorldManager.cs
+++ unityproj/Assets/Gameplay/Gameplay/WorldManager.cs
@@ -6,6 +6,10 @@ public class WorldManager : MonoBehaviour {
 	public AudioClip worldBGM;
 	
 	void Start () {
+		// always reset for a new round
+		Time.timeScale = 1.0f;
+		Time.fixedDeltaTime = 0.02f;
+			
 		// play loop audio
 		audio.clip = worldBGM;
 		audio.loop = true;
--- unityproj/Assets/game_assets/GUIGame.cs
+++ unityproj/Assets/game_assets/GUIGame.cs
@@ -11,6 +11,7 @@ public class GUIGame : MonoBehaviour {
     {
         int sec = (int)((float)timeInMillseconds / 1000F);
         int msec = timeInMillseconds % 1000;
+		
         GUI.TextField(new Rect(Screen.width - 210, 0, 210, 50),
             string.Format("Time To Live: {0:0}:{1:000}", sec, msec  ) , timerStyle);
 		GUI.TextField (new Rect(Screen.width - 210, 30, 210, 50),
